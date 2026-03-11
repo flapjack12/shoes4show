afff76bdc7038417040622a74399c97efae6fb1e
Danylo Sadovyy <3026466s@student.gla.ac.uk>
2026-03-11 23:14:52 +0000

    password for db

--- shoes4show_project/settings.py
+++ shoes4show_project/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         'NAME': 'defaultdb',
         'USER': 'avnadmin',
-        'PASSWORD': '',
+        'PASSWORD': 'AVNS_WpX8Q6s5TBOJ7hi1KB7',
         'HOST': 'pg-2355ee8e-sadovyy.i.aivencloud.com',
         'PORT': '16618'
     }
