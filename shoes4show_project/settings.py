4fe22640011c11aca49e8da521085ff9b2a62048
Danylo Sadovyy <3026466s@student.gla.ac.uk>
2026-03-11 23:05:35 +0000

    removed secret

--- shoes4show_project/settings.py
+++ shoes4show_project/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         'NAME': 'defaultdb',
         'USER': 'avnadmin',
-        'PASSWORD': 'AVNS_WpX8Q6s5TBOJ7hi1KB7',
+        'PASSWORD': '',
         'HOST': 'pg-2355ee8e-sadovyy.i.aivencloud.com',
         'PORT': '16618'
     }
