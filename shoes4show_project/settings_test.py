753ebe66aa6a0c26786b5d2209db4f337aa88bac
SadovyyD <134547586+SadovyyD@users.noreply.github.com>
2026-03-26 11:31:38 +0000

    Update settings_test.py

--- shoes4show_project/settings_test.py
+++ shoes4show_project/settings_test.py
@@ -2,7 +2,12 @@ from .settings import *
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": ":memory:",
+        "ENGINE": "django.db.backends.postgresql",
+        'NAME': 'defaultdb',
+        'USER': 'avnadmin',
+        'PASSWORD': 'AVNS_WpX8Q6s5TBOJ7hi1KB7',
+        'HOST': 'pg-2355ee8e-sadovyy.i.aivencloud.com',
+        'PORT': '16618'
     }
 }
+
